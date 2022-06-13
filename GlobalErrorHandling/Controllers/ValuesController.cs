98116fdb874802ea05fbcc4e6d2d8646f6f16fa7
Kefas Ogabi <kefasogabi@gmail.com>
2022-06-13 10:26:54 +0100

    error handler

---
+++ GlobalErrorHandling/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalErrorHandling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogInfo("Fetching all students from the storage");
+
+            var students = DataManager.GetAllStudents();
+
+            throw new Exception("Exception while fetching all students from the storage");
+
+            _logger.LogInfo($"Returning {students.Count} students");
+
+            return Ok(students);
+        }
+    }
+}
---
+++ GlobalErrorHandling/CustomExceptionMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using GlobalErrorHandling.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GlobalErrorHandling.CustomExceptionMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerManager _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }catch(Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+
+                await HandleExceptionAsync(httpContext);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(new ErrorDetails
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal server error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
